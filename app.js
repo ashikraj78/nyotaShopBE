8096087de0d35c141e0352f445135972052d3b84
Ashik Raj <ashikraj.78@gmail.com>
2023-07-25 20:25:16 +0530

    adding all origin allowed for the vercel front end

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
