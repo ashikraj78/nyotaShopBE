588bafe4318cafa4cdf667081a891912e9e7945c
Ashik Raj <ashikraj.78@gmail.com>
2023-08-16 21:11:17 +0530

    adding updating formData

--- routes/formData.js
+++ routes/formData.js
@@ -4,5 +4,6 @@ const formDataController = require("../src/formData/formDataController");
 
 // create formData
 router.post("/createFormData", formDataController.createFormData);
+router.put("/updateFormData", formDataController.updateFormData);
 
 module.exports = router;
--- src/formData/formDataController.js
+++ src/formData/formDataController.js
@@ -32,6 +32,41 @@ let formDataController = {
       return res.status(500).json({ msg: "Failed to create formData" });
     }
   },
+  updateFormData: async function (req, res, next) {
+    let formDataId = req.query.id;
+    const {
+      userId,
+      productId,
+      brideData,
+      groomData,
+      eventsData,
+      images,
+      specialNotes,
+    } = req.body;
+    const updatedFormData = {
+      userId: userId,
+      productId: productId,
+      brideData: brideData,
+      groomData: groomData,
+      eventsData: eventsData,
+      specialNotes: specialNotes,
+      images: images,
+    };
+    if (!userId || !productId || !brideData || !groomData || !eventsData) {
+      return res.status(400).json({
+        msg: "userId ,productId, brideData, groomData and eventsData  is required",
+      });
+    }
+    try {
+      const formData = await formDataServices.updateFormData(
+        formDataId,
+        updatedFormData
+      );
+      return res.json(formData);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
 module.exports = formDataController;
